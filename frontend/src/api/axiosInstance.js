b10b5efbd8a9fb95ae67207fa17e024f8d14c0ac
Kalyan-71 <164600184+Kalyan-71@users.noreply.github.com>
2025-05-06 19:01:28 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://noise-backend.onrender.com/api',
   withCredentials: true // important to include cookies
 });
 
